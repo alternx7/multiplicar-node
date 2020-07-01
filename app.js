c804458f2221b6d3257bb462db5e505aefd24969
X <alternx7@yahoo.com>
2020-07-01 03:49:41 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,30 @@
+//No lleva directorio porque es un paquete definido en los módulos
+
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar.js')
+
+let comando = argv._[0];
+
+switch (comando) {
+
+    case 'listar':
+        listarTabla(argv.base, argv.limite)
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${colors.green(archivo)}`))
+            .catch(err => console.log(err));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+
+}
+
+//console.log('Limite', argv.limite);
+
+//let parametro = argv[2];
+//let base = parametro.split('=')[1];
